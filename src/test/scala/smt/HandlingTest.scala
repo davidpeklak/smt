65517359ac17673d0f90bdf9afa192a5b5ac72b9
David Peklak <dpeklak@gmail.com>
2016-03-25 10:25:08 +0100

    all tests pass

--- src/test/scala/smt/HandlingTest.scala
+++ src/test/scala/smt/HandlingTest.scala
@@ -6,10 +6,6 @@ import org.scalacheck.Gen
 import smt.report.Reporter
 import scalaz.{\/-, \/}
 import sbt.{Level, Logger}
-import smt.report.ReportersAction.HasReporters
-import smt.describe.DescribeAction.HasLogger
-import smt.db.DbAction.HasDb
-import smt.db.AddAction.{HasUser, HasRemark}
 
 class HandlingTest extends FunSuite {
 
@@ -29,14 +25,6 @@ class HandlingTest extends FunSuite {
     def success(message: => String): Unit = ()
   }
 
-  lazy val handling = new Handling[HandlingDep] {
-    lazy val hasDb: HasDb[HandlingDep] = _.db
-    lazy val hasLogger: HasLogger[HandlingDep] = _.logger
-    lazy val hasReporters: HasReporters[HandlingDep] = _.rps
-    lazy val hasUser: HasUser[HandlingDep] = _.user
-    lazy val hasRemark: HasRemark[HandlingDep] = _.remark
-  }
-
   test("apply one migration - verify reporter is called") {
 
     val mig = migGen.apply(Gen.Params()).get // bochn
@@ -45,9 +33,7 @@ class HandlingTest extends FunSuite {
 
     val logger = new LoggerMock
 
-    val action = handling.applyMigrationsAndReport(ms = Seq(mig), imo = None, arb = false, runTests = true)
-
-    action.run(HandlingDep(new DatabaseMock(new ConnectionMock), List(reporter), logger, "fooUser", None)).run
+    Handling.applyMigrationsAndReport(ms = Seq(mig), imo = None, arb = false, runTests = true, "user", "remark")(new DatabaseMock(new ConnectionMock), logger, List(reporter), new NamedMoveStatesHolder())
 
     assert(reporter.called)
   }
